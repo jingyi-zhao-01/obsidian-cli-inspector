13aee1d06c9c1651c581f600fffd83159a29e668
Jingyi Zhao <zhao.elton@gmail.com>
2026-03-07 04:04:11 -0800

    chore: commit lint ignore dependant bot

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,6 @@
 export default {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  ignores: [
+    (message) => message.includes('dependabot')
+  ]
 };
